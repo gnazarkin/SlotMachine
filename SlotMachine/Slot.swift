34a9b8b32036a503ad56247f979447e90e22998c
gnazarkin <gleb.nazarkin@gmail.com>
2014-12-24 20:17:22 -0500

    Finished slot machine

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Gleb Nazarkin on 2014-12-18.
+//  Copyright (c) 2014 Addo Labs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
